1d454911d290932e850982a0a8d44ff31e7b5aea
Thomas M.G <tgrose17@student.aau.dk>
2018-04-12 11:50:30 +0200

    Added variables, constructors, getters and setters for NightSky Added tests for all getters and setters for NightSky

--- test/dk/aau/sw2_18_a305/nightsky/NightSkyTest.java
+++ test/dk/aau/sw2_18_a305/nightsky/NightSkyTest.java
@@ -1,7 +1,37 @@
 package dk.aau.sw2_18_a305.nightsky;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NightSkyTest {
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void getWidth() {
+    }
+
+    @Test
+    void getStars() {
+    }
+
+    @Test
+    void getConstellations() {
+    }
+
+    @Test
+    void setStars() {
+    }
+
+    @Test
+    void setConstellations() {
+    }
 }
