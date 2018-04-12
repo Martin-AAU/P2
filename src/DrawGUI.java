8177c0d057d8c725f2910972a77a485cde48b81b
Thomas M.G <tgrose17@student.aau.dk>
2018-04-12 14:57:20 +0200

    Changes in DrawGUI

--- src/DrawGUI.java
+++ src/DrawGUI.java
@@ -4,14 +4,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class DrawGUI extends Application {
-    NightSky nightSky = new NightSky(1024, 768);
 
     public static void main(String[] args) {
         launch(args);
     }
 
     public void start(Stage primaryStage) {
+        NightSky nightSky = new NightSky(1024, 768);
+        nightSky.generateStars(100);
+
         primaryStage.setTitle("NightSky");
         primaryStage.setScene(new Scene(nightSky, 1024, 768));
+
+        primaryStage.show();
     }
 }
