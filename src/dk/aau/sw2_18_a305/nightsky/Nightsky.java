17bef798e3bf197c976d3757f2190123ba9c4c48
Thomas M.G <tgrose17@student.aau.dk>
2018-04-17 10:16:21 +0200

    Change Nightsky1 to Nightsky

--- src/dk/aau/sw2_18_a305/nightsky/Nightsky.java
+++ src/dk/aau/sw2_18_a305/nightsky/Nightsky.java
@@ -2,16 +2,16 @@ package dk.aau.sw2_18_a305.nightsky;
 
 import java.util.ArrayList;
 
-public class Nightsky1 {
+public class Nightsky {
     private ArrayList<Star> stars = new ArrayList<>();
     private ArrayList<Constellation> constellations = new ArrayList<>();
 
     //CONSTRUCTORS
-    public Nightsky1() { }
-    public Nightsky1(ArrayList<Star> stars) {
+    public Nightsky() { }
+    public Nightsky(ArrayList<Star> stars) {
         this.stars = stars;
     }
-    public Nightsky1(ArrayList<Star> stars, ArrayList<Constellation> constellations) {
+    public Nightsky(ArrayList<Star> stars, ArrayList<Constellation> constellations) {
         this.stars = stars;
         this.constellations = constellations;
     }
