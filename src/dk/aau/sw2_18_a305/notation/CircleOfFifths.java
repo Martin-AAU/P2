10653e01db08c5b6aad3142007d654eaf807cafe
Martin-AAU <32766647+Martin-AAU@users.noreply.github.com>
2018-04-26 15:45:58 +0200

    Changed methodname "distanceto" to "distanceTo"

--- src/dk/aau/sw2_18_a305/notation/CircleOfFifths.java
+++ src/dk/aau/sw2_18_a305/notation/CircleOfFifths.java
@@ -2,7 +2,7 @@ package dk.aau.sw2_18_a305.notation;
 
 public class CircleOfFifths implements DistanceStrategy {
     @Override
-    public int distanceto(Chord from, Chord to) {
+    public int distanceTo(Chord from, Chord to) {
         return 0;
     }
 }
--- src/dk/aau/sw2_18_a305/notation/DistanceStrategy.java
+++ src/dk/aau/sw2_18_a305/notation/DistanceStrategy.java
@@ -1,5 +1,5 @@
 package dk.aau.sw2_18_a305.notation;
 
 public interface DistanceStrategy {
-    int distanceto(Chord from, Chord to);
+    int distanceTo(Chord from, Chord to);
 }
