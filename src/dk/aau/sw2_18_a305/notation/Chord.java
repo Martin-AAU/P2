006627aa6a44788ba357cb15e975e398d0021852
Gamped <mathiasgam@gmail.com>
2018-05-15 11:37:02 +0200

    Added getChordType function

--- src/dk/aau/sw2_18_a305/notation/Chord.java
+++ src/dk/aau/sw2_18_a305/notation/Chord.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 import static dk.aau.sw2_18_a305.notation.PitchClass.*;
 
-
+// NOTE: Interval is defined as distance from prior note
 public class Chord{
 
     private DistanceStrategy distanceStrategy = new CircleOfFifths();
@@ -48,6 +48,34 @@ public class Chord{
         return notes;
     }
 
+    // Looks through every interval and returns the type of chord
+    // Source of intervals: https://www.edmprod.com/different-chord-types/
+    public ChordType getChordType() {
+        if (intervals.size() == 2 && intervals.get(0) == 4 && intervals.get(1) == 3){
+            return ChordType.Major;
+        } else if (intervals.size() == 2 && intervals.get(0) == 3 && intervals.get(1) == 4){
+            return ChordType.Minor;
+        } else if (intervals.size() == 2 && intervals.get(0) == 3 && intervals.get(1) == 3){
+            return ChordType.Diminshed;
+        } else if (intervals.size() == 3 && intervals.get(0) == 4 && intervals.get(1) == 3 && intervals.get(2) == 7){
+            return ChordType.Major7;
+        } else if (intervals.size() == 3 && intervals.get(0) == 3 && intervals.get(1) == 4 && intervals.get(2) == 3){
+            return ChordType.Minor7;
+        } else if (intervals.size() == 3 && intervals.get(0) == 4 && intervals.get(1) == 3 && intervals.get(2) == 3){
+            return ChordType.Dominant7;
+        } else if (intervals.size() == 2 && intervals.get(0) == 2 && intervals.get(1) == 5){
+            return ChordType.Suspended2;
+        } else if (intervals.size() == 2 && intervals.get(0) == 5 && intervals.get(1) == 2){
+            return ChordType.Suspended4;
+        } else if (intervals.size() == 2 && intervals.get(0) == 4 && intervals.get(1) == 4){
+            return ChordType.Augmented;
+        } else if (intervals.size() == 4 && intervals.get(0) == 4 && intervals.get(1) == 3 && intervals.get(2) == 3 && intervals.get(3) == 4){
+            return ChordType.Dominant9;
+        } else if (intervals.size() == 5 && intervals.get(0) == 4 && intervals.get(1) == 3 && intervals.get(2) == 4 && intervals.get(3) == 3 && intervals.get(4) == 3){
+            return ChordType.Major11;
+        } else return ChordType.UNKNOWN;
+    }
+
     // SETTERS
     public void setDistanceStrategy(DistanceStrategy distanceStrategy) {
         this.distanceStrategy = distanceStrategy;
@@ -61,7 +89,7 @@ public class Chord{
 
     private Note setNote(int interval) {
 
-        PitchClass p = null;
+        PitchClass p;
         int index = notes.get(notes.size()-1).getPitchClass().number + interval;
         int octave = notes.get(0).getOctave() + (index/12);
 
--- src/dk/aau/sw2_18_a305/notation/ChordType.java
+++ src/dk/aau/sw2_18_a305/notation/ChordType.java
@@ -1,5 +1,5 @@
 package dk.aau.sw2_18_a305.notation;
 
 public enum ChordType {
-    Major, Minor, Diminshed, Suspended2, Suspended4, Augmented
+    Major, Minor, Diminshed, Major7, Minor7, Dominant7, Suspended2, Suspended4, Augmented, Dominant9, Major11, UNKNOWN
 }
