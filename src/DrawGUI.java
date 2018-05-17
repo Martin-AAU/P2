4732db3160de7e07d38492915dd6121ca1783fbf
Thomas M.G <tgrose17@student.aau.dk>
2018-05-17 09:40:58 +0200

    Fixed GUI borders

--- src/DrawGUI.java
+++ src/DrawGUI.java
@@ -45,7 +45,7 @@ public class DrawGUI extends Application {
         nightsky.addConstellation(new Constellation("Music Constellation"));
 
         for(int i = 0; i < 50; i++) {
-            nightsky.addStar(new Star(random.nextInt(width-16)+8, random.nextInt(height-16)+8));
+            nightsky.addStar(new Star(random.nextInt(width-30)+15, random.nextInt(height-30)+15));
         }
 
         // Creating buttons
@@ -98,7 +98,7 @@ public class DrawGUI extends Application {
 
         // Generates circles and lines from the stars x and y positions
         for(int i = 0; i < stars.size(); i++) {
-            Circle c = new Circle(stars.get(i).getxCoordinate(), stars.get(i).getyCoordinate(), 7+ random.nextInt(8), Color.WHITE);
+            Circle c = new Circle(stars.get(i).getxCoordinate(), stars.get(i).getyCoordinate(), 7 + random.nextInt(8), Color.WHITE);
             int finalI = i;
 
             c.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
